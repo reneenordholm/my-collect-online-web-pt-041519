5143b887f83b7acc4af7d27c01a285d30cec53d1
Renee Nordholm <reneenordholm@gmail.com>
2019-04-26 15:50:38 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(arg)
   i = 0
   collection = []
   while i < arg.length
-    collection << yield arg[1].upcase
+    collection << yield(arg[1].upcase)
       i += 1
  end
  collection
